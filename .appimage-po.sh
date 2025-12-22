f3d476d15b9760e8d3424b9596bb414c425d3cc8
nick <noobping@users.noreply.github.com>
2025-12-22 16:01:26 +0100

    integrate po script in pipeline

---
+++ .appimage-po.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+APP_ID=io.github.noobping.listenmoe
+OUTDIR="${1:-AppDir/share/locale}"
+
+for f in po/*.po; do
+    lang=$(basename "$f" .po)
+    mkdir -p "$OUTDIR/$lang/LC_MESSAGES"
+    msgfmt "$f" -o "$OUTDIR/$lang/LC_MESSAGES/$APP_ID.mo"
+done
